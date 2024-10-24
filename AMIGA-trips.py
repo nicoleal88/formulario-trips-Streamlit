d9e7d0312ea4d6bf2b8ef683958b16026084177b
nicoleal88 <nicoleal88@gmail.com>
2024-10-24 17:11:09 -0300

    added log in page

--- AMIGA-trips.py
+++ AMIGA-trips.py
@@ -9,6 +9,48 @@ from PIL import Image
 import io
 import time
 
+import hmac
+
+
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def login_form():
+        """Form with widgets to collect user information"""
+        with st.form("Credentials"):
+            st.header("Operations and monitoring - UMD", divider="grey")
+            st.text_input("Username", key="username")
+            st.text_input("Password", type="password", key="password")
+            st.form_submit_button("Log in", on_click=password_entered)
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["username"] in st.secrets[
+            "passwords"
+        ] and hmac.compare_digest(
+            st.session_state["password"],
+            st.secrets.passwords[st.session_state["username"]],
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+            # del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the username + password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show inputs for username + password.
+    login_form()
+    if "password_correct" in st.session_state:
+        st.error("😕 User not known or password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()
+
 # Initialize or get the language from session state
 if 'language' not in st.session_state:
     st.session_state['language'] = 'es'  # Default language is Spanish
