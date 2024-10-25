0914d4b168790bf880847d5baf73d4b8821230ba
nicoleal88 <nicoleal88@gmail.com>
2024-10-25 09:08:35 -0300

    deleted username from session_state

--- AMIGA-trips.py
+++ AMIGA-trips.py
@@ -32,8 +32,8 @@ def check_password():
             st.secrets.passwords[st.session_state["username"]],
         ):
             st.session_state["password_correct"] = True
-            del st.session_state["password"]  # Don't store the password.
-            # del st.session_state["username"]
+            del st.session_state["password"]  # Don't store the username and password.
+            del st.session_state["username"]
         else:
             st.session_state["password_correct"] = False
 
